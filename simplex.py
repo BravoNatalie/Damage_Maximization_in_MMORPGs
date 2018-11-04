2465a182dcff3ddaa798fb8698075d8191f1348d
Lohan-Ferreira <lohanext@gmail.com>
2018-11-04 09:50:11 -0200

    Codigo Completo e Instancia Exemplo

--- simplex.py
+++ simplex.py
@@ -6,8 +6,11 @@ import csv
 
 infile = open("data.txt",'r')
 
-reader = csv.reader(open("spells.csv","rb"),delimiter=";")
+reader = csv.reader(open("spells.csv","r"),delimiter=";")
 l = list(reader)
+
+#A matriz de feitiços contem as magias nas primeiras linhas que vão de 0 a n_spells-1
+#A partir de n_spells em diante, cada linha é composta por [x][0] = ID da magia desencadeadora  e [x][y] = IDs de magias desencadeadas ( y>0 )
 spells = numpy.array(l).astype("float")
 
 
@@ -20,6 +23,13 @@ TT = int(lines[3])
 infile.close()
 var = []
 
+print(spells)
+
+
+#Setting FMi to default spells
+for x in range(0,n_spells):
+    if(spells[x][7] < 0):
+        spells[x][7] = (1.0/(spells[x][1]+spells[x][3]))
 
 
 try:
@@ -33,15 +43,41 @@ try:
     # Set objective
     expr = LinExpr()
     for x in range(0,n_spells):
-        expr += var[x]*spells[x][1]/TT  #Define somatorio onde spells[x][1] = Dmgx
+        expr += var[x]*spells[x][2]/TT  #Define somatorio onde spells[x][1] = Dmgx
     m.setObjective(expr,GRB.MAXIMIZE)   
-
-    # Add constraint: x + 2 y + 3 z <= 4
-    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
-
-    # Add constraint: x + y >= 1
-    m.addConstr(x + y >= 1, "c1")
-
+    
+    # Add constraints:
+    cc = 1  #Constraints counter 
+    expr = LinExpr()
+    for x in range(0,n_spells):
+        expr += var[x]*(spells[x][1]+spells[x][3])     #Define somatorio tal que sum(Ni*(Casti+CDi)) <= TT 
+    m.addConstr(expr <= TT , "c" + str(cc))
+    cc += 1
+    
+    for x in range(0,n_spells):
+        m.addConstr(var[x]/TT >= spells[x][6],"c"+str(cc))#Define Ni/TT >= Fmi
+        cc += 1
+    for x in range(0,n_spells):
+        m.addConstr(var[x]/TT <= spells[x][7],"c"+str(cc))#Define Ni/TT <= FMi
+        cc += 1
+    expr = LinExpr()
+    for x in range(0,n_spells):
+        expr += var[x] * (spells[x][4]/TT)
+    m.addConstr(expr <= MRR, "c" + str(cc))                     #Define Sum(i) Ni*(MPi/TT) <= MRR
+    cc += 1
+    expr = LinExpr()
+    for x in range(0,n_spells):
+        expr += var[x] * (spells[x][5]/TT)
+    m.addConstr(expr >= TF, "c" + str(cc))                      #Define Sum(i) Ni*(HPi/TT) >= TF
+    cc += 1
+    for x in range (n_spells, len(spells)):                     #Define Ni >= Nj se i desencadeia j 
+        for y in range(1, len(spells[x])):
+            if(spells[x][y] > -0.00001):
+                m.addConstr(var[int(spells[x][0])] >= var[int(spells[x][y])],"c"+str(cc))
+                cc += 1
+    print(var)
+    m.write("model.lp")
+    
     m.optimize()
 
     for v in m.getVars():
@@ -54,3 +90,4 @@ except GurobiError as e:
 
 except AttributeError:
     print('Encountered an attribute error')
+
